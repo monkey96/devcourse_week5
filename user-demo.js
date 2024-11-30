16c19864880aca9aa90c22a26744d6d267ff7c67
monkey96 <suminp12@gmail.com>
2024-12-01 01:04:26 +0900

    WEEK 5, Day 1 example code. Did not follow through with the routing since it only clustter the code at the moment.

---
+++ user-demo.js
@@ -0,0 +1,62 @@
+// express module setting
+const express = require('express');
+const app = express();
+app.listen(5500);
+app.use(express.json())
+
+let db = new Map();
+
+app.post('/login', function(req, res) {
+    const {id, pwd} = req.body;
+
+    res.json({
+        message: `Welcome User ${name}`
+    });
+})
+
+app.post('/join', function(req, res) {
+    const {id, pwd, name} = req.body;
+
+    if (id != undefined && pwd != undefined && name != undefined) {
+        db.set(id, req.body);
+        res.status(201).json({
+            message: `Welcome User ${name}`
+        });
+    }
+    else {
+        res.status(400).json({
+            message: `Unable to process your request`
+        });
+    }
+})
+
+app.get('/users/:id', function(req, res) {
+    let {id} = req.params;
+    const user = db.get(id);
+    if (user) {
+        res.status(200).json({
+            userId : user.id,
+            name : user.name
+        });
+    } else {
+        res.status(404).json({
+            message: `Unable to find user with id ${id}`
+        })
+    }
+
+})
+
+app.delete('/users/:id', function(req, res) {
+    let {id} = req.params;
+    const user = db.get(id);
+    if (user) {
+        res.status(200).json({
+            message: `Successfully removed user id ${user.id}`
+        });
+        db.delete(id);
+    } else {
+        res.status(404).json({
+            message: `Unable to find user with id ${id}`
+        })
+    }
+})
